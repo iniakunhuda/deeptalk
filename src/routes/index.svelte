c4e6d1d33dfe80b425701b2f4dbae2b440d762a6
Huda <iniakunhuda@gmail.com>
2021-12-01 02:17:31 +0700

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,42 @@
+<script>
+import talk from './talk.json';
+
+const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+let max_num = talk.q.length;
+let num = random(0, max_num);
+</script>
+
+<title>
+    kita.hebat
+</title>
+
+<div class="py-12 bg-white">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:mt-40 sm:mt-40 mt-3">
+      <div class="text-center">
+        <p class="absolute inset-x-0 top-5 text-gray-700">
+            <a href="/tentang"><u>Tentang</u></a>
+        </p>
+        <!-- <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Deeptalk</h2> -->
+        <div class="stack">
+            <div class="text-center border border-base-content card bg-base-100">
+              <div class="card-body">
+                <p class="mt-2 leading-8 font-light tracking-tight text-gray-900 text-2xl">
+                    { talk.q[num] }
+                </p>
+              </div>
+            </div> 
+            <div class="text-center border border-base-content card bg-base-100">
+              <div class="card-body">B</div>
+            </div> 
+            <div class="text-center border border-base-content card bg-base-100">
+              <div class="card-body">C</div>
+            </div>
+        </div>
+        <p class="absolute inset-x-0 bottom-5 text-gray-400">
+            <span>Tulis dan bagikan sudut pandangmu ke Dunia</span>
+        </p>
+      </div>
+
+    </div>
+</div>
+
